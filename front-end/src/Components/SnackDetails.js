5fe7a9cc69e123d1d00c39dd0f300ff31b047bd0
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-15 11:54:17 -0500

    Updated the code to render each code as well as the .env Port

--- front-end/src/Components/SnackDetails.js
+++ front-end/src/Components/SnackDetails.js
@@ -2,48 +2,51 @@ const { default: axios } = require("axios");
 const { useState, useEffect } = require("react");
 const { useNavigate, useParams, Link } = require("react-router-dom");
 
-function SnackDetails () {
-    const API = process.env.REACT_API;
-    const navigate = useNavigate();
-    const { id }  = useParams();
-    const [snack, setSnack] = useState({});
+function SnackDetails() {
+  const API = process.env.REACT_APP_API_URL;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [snack, setSnack] = useState({});
 
+  useEffect(() => {
+    axios
+      .get(`${API}/snacks/${id}`)
+      .then((response) => setSnack(response.data.payload))
+      .catch((err) => console.warn(err));
+  }, [API, id]);
 
-useEffect(() => {
-    axios.get(`${API}/snacks/${id}`)
-    .then((response) => setSnack(response.data))
-    .catch((err) => console.warn(err))
-},[API, id])
+  const handleDelete = () => {
+    axios
+      .delete(`${API}/snacks/${id}`)
+      .then(
+        () => {
+          console.log("You go Girl");
+          navigate("/snacks");
+        },
+        (err) => console.error(err)
+      )
+      .catch((err) => console.warn(err));
+  };
 
-
-const handleDelete = () => {
-    axios.delete(`${API}/snacks/${id}`).then(() => {
-        console.log("You go Girl");
-        navigate("/snacks");
-    }, (err) => console.error(err)).catch((err) => console.warn(err));
-};
-
-
-
-return (
+  return (
     <article className="snack-detail">
-        <h4>Snack Name: {snack.name}</h4>
-        <div className="showNavigation">
+      <h4>Snack Name: {snack.name}</h4>
+      <div className="showNavigation">
         <div>
-            <Link to="/snacks">
+          <Link to="/snacks">
             <button>Back</button>
-            </Link>
+          </Link>
         </div>
         <div>
-            <Link to={`/snacks/${snack.id}/edit`}>
+          <Link to={`/snacks/${snack.id}/edit`}>
             <button>Edit</button>
-            </Link>
+          </Link>
         </div>
         <div>
-            <button onClick={handleDelete}>Delete</button>
+          <button onClick={handleDelete}>Delete</button>
         </div>
-        </div>
+      </div>
     </article>
-    )
+  );
 }
 export default SnackDetails;
