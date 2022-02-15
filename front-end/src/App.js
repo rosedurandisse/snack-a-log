7ef95833fbb1f8a441b04af76bead7c249c2f0ea
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-15 11:09:13 -0500

    Adding the listed components, pages, routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,16 +3,18 @@ import NavBar from "./Components/NavBar";
 import Edit from "./Pages/Edit";
 import Home from "./Pages/Home";
 import Show from "./Pages/Show";
+import Index from "./Pages/Index";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
       <main>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/snacks/:id" element={<Show/>}/>
-          <Route path="/snacks/:id/edit" element={<Edit/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/snacks" element={<Index />} />
+          <Route path="/snacks/:id" element={<Show />} />
+          <Route path="/snacks/:id/edit" element={<Edit />} />
         </Routes>
       </main>
     </div>
