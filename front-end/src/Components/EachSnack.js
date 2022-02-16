7184b805e491f07f5c91c20a5cb9454c56bab9c9
Eva Rodriguez <evarodriguez@Evas-MBP.lan>
2022-02-16 12:46:14 -0500

    Have a Functional App

--- front-end/src/Components/EachSnack.js
+++ front-end/src/Components/EachSnack.js
@@ -1,47 +1,15 @@
-import heartSolid from "../assets/heart-solid.png";
-import heartOutline from "../assets/heart-regular.png";
 import { Link } from "react-router-dom";
+import HeartHealth from "./HeartHealth";
 
 function Snack({ snack }) {
-  const healthy = snack.is_healthy;
 
   return (
-    <tr>
-      <div className="Snack">
-        <img src={snack.image} alt="snacks"></img>
-
-        {/* <h4 className="Snack"> */}
-        <Link to={`/snacks/${snack.id}`}></Link>
-        <h4>
-          {" "}
-          {snack.name}
-          {healthy ? (
-            <h4>
-              {" "}
-              <img src={heartSolid} alt="healthy food" />{" "}
-            </h4>
-          ) : (
-            <h4>
-              {" "}
-              <img src={heartOutline} alt="unhealthy food" />{" "}
-            </h4>
-          )}{" "}
-        </h4>
-        {/* </h4> */}
-
-        {/* <h4>
-          {snack.is_healthy} ?{" "}
-          <img src={heartSolid} alt="healthy food">
-            /
-          </img>{" "}
-          :
-          <img src={heartOutline} alt="UNhealthy food">
-            /
-          </img>
-        </h4> */}
-      </div>
-      <td>{/* <img src={snack.image} alt="healthy food"></img> */}</td>
-    </tr>
+      <h4 className="Snack">
+        {snack.name}
+        <div></div>
+        <Link to={`/snacks/${snack.id}`}><img src={snack.image} alt="snacks"></img></Link>
+        <h4>{<HeartHealth snackHealth={snack.is_healthy} />}</h4>
+      </h4>
   );
 }
 
